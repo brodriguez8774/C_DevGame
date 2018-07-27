09e855c075cb9668ec3de1c14708ca715d282479
Brandon Rodriguez <brodriguez8774@gmail.com>
2018-07-27 07:23:29 -0400

    Render a single triangle

--- main.c
+++ main.c
@@ -35,12 +35,29 @@
 
 
 // Variable Declaration.
+// Shaders copied from https://learnopengl.com/Getting-started/Hello-Triangle
+const char* VERTEX_SHADER_SOURCE = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n\n"
+"void main() {\n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"}\n\0 ";
+const char* FRAGMENT_SHADER_SOURCE = "#version 330 core\n"
+"out vec4 FragColor;\n\n"
+"void main() {\n"
+"    FragColor = vec4(0.0f, 0.0f, 1.0f, 1.0f);\n"
+"}\n\0 ";
 
 
 // Method Declaration.
-void error_callback(int error, const char* err_description);
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
-void framebuffer_size_callback(GLFWwindow*, int width, int height);
+unsigned int set_up_shaders(unsigned int);
+unsigned int create_vertex_shader(unsigned int);
+unsigned int create_fragment_shader(unsigned int);
+void render_data(unsigned int);
+// Callbacks.
+void error_callback(int, const char*);
+void key_callback(GLFWwindow*, int, int, int, int);
+void framebuffer_size_callback(GLFWwindow*, int, int);
+
 
 
 /**
@@ -48,8 +65,9 @@ void framebuffer_size_callback(GLFWwindow*, int width, int height);
  * Initializes and runs program.
  */
 int main(int argc, char* argv[]) {
-    GLFWwindow* window;
-    static const GLfloat window_background[] = {0.0f, 0.0f, 0.0f, 1.0f};
+    GLFWwindow* window;                 // The main window object to render on.
+    static const GLfloat window_background[] = {0.2f, 0.2f, 0.3f, 1.0f};
+    unsigned int shader_program = 0;    // A unique id ref to set of linked shaders to render with.
 
     printf("\nStarting program.\n");
 
@@ -90,11 +108,14 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
+    // Set up rendering shaders.
+    shader_program = set_up_shaders(shader_program);
+
     // Main loop for window rendering.
     while (! glfwWindowShouldClose(window) ) {
         glClearBufferfv(GL_COLOR, 0, window_background);
 
-        // Clear buffer for new render
+        render_data(shader_program);
 
         // Check/call events and swap buffers.
         glfwSwapBuffers(window);
@@ -108,6 +129,118 @@ int main(int argc, char* argv[]) {
 
     exit(0);
 }
+
+
+/**
+ *  Establish shaders to render GPU visuals.
+ *  Visuals are processed through shaders in order of:
+ *      * Vertex Shaders
+ *      * Geometry Shaders
+ *      * Fragment Shaders
+ *  Note that a basic geometry shader is provided by default, so creating a new one is optional.
+ */
+unsigned int set_up_shaders(unsigned int shader_program) {
+    unsigned int vertex_shader = 0;     // A unique id relating to the vertex shader.
+    unsigned int fragment_shader = 0;   // A unique id relating to the fragment shader.
+
+    // Create shaders.
+    vertex_shader = create_vertex_shader(vertex_shader);
+    fragment_shader = create_fragment_shader(fragment_shader);
+
+    // Link shaders.
+    shader_program = glCreateProgram();
+    glAttachShader(shader_program, vertex_shader);
+    glAttachShader(shader_program, fragment_shader);
+    glLinkProgram(shader_program);
+
+    // Delete old references to shaders.
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return shader_program;
+}
+
+
+/**
+ * Compiles vertex shader.
+ */
+unsigned int create_vertex_shader(unsigned int vertex_shader) {
+    int success;
+    char info_log[512];
+
+    // Initialize.
+    vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &VERTEX_SHADER_SOURCE, NULL);
+    glCompileShader(vertex_shader);
+
+    // Check for errors.
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (! success) {
+        glGetShaderInfoLog(vertex_shader, 512, NULL, info_log);
+        fprintf(stderr, "Error compiling vertex shader: %s\n", info_log);
+    }
+
+    return vertex_shader;
+}
+
+
+/**
+ * Compiles fragment shader.
+ */
+unsigned int create_fragment_shader(unsigned int fragment_shader) {
+    int success;
+    char info_log[512];
+
+    // Initialize.
+    fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &FRAGMENT_SHADER_SOURCE, NULL);
+    glCompileShader(fragment_shader);
+
+    // Check for errors.
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (! success) {
+        glGetShaderInfoLog(fragment_shader, 512, NULL, info_log);
+        fprintf(stderr, "Error compiling fragment shader: %s\n", info_log);
+    }
+
+    return fragment_shader;
+}
+
+
+/**
+ * Basic example of attempting to render a triangle.
+ */
+void render_data(unsigned int shader_program) {
+    unsigned int vertex_buffer_object;
+    unsigned int vertex_array_object;
+    float verticies[] = {
+        0.0f, 0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f
+    };
+
+    // Generate array object and buffers.
+    glGenVertexArrays(1, &vertex_array_object);
+    glGenBuffers(1, &vertex_buffer_object);
+
+    // Add vertex data to graphics buffer. Start with array object, then vertex buffers.
+    glBindVertexArray(vertex_buffer_object);
+    glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer_object);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+    // Set vertix attribute pointers?
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, (3 * sizeof(float)), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Use shader program when rendering.
+    glUseProgram(shader_program);
+
+    // Draw object.
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
 
 
 /**
